8dd170e5ce20ec18a592b09dc615e9b1f345248b
Melvin Gerardo <mgerardo@schools.ph>
2023-01-05 15:20:54 +0800

    Setup

--- resources/views/template/page.blade.php
+++ resources/views/template/page.blade.php
@@ -7,7 +7,6 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link type="text/css" rel="stylesheet" href="{{ asset("css/app.css") }}">
     <link type="text/css" rel="stylesheet" href="{{ asset("css/bootstrap.min.css") }}">
-    {{-- <link type="text/css" rel="stylesheet" href="{{ asset("css/bootstrap.css") }}"> --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body >
